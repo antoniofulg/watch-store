ebb893237f181bb25d0fa21a4bd9944cbe03a145
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-10-24 18:22:26 -0300

    feature: implemented addToCart emit method with TDD

---
+++ components/ProductCard.unit.spec.js
@@ -0,0 +1,57 @@
+import { mount } from '@vue/test-utils'
+import ProductCard from '@/components/ProductCard.vue'
+import { makeServer } from '@/miragejs/server'
+
+const mountProductCard = () => {
+  const product = server.create('product', {
+    title: 'Relógio bonito',
+    price: '22.00',
+    image:
+      'https://images.unsplash.com/photo-1434056886845-dac89ffe9b56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  })
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        product,
+      },
+    }),
+    product,
+  }
+}
+
+describe('ProductCard - unit', () => {
+  let server
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' })
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+
+  it('should match snapshot', () => {
+    const { wrapper } = mountProductCard()
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('should mount the component', () => {
+    const { wrapper } = mountProductCard()
+
+    console.log(wrapper.html())
+
+    expect(wrapper.vm).toBeDefined()
+    expect(wrapper.text()).toContain('Relógio bonito')
+    expect(wrapper.text()).toContain('$22.00')
+  })
+
+  it('shoul emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard()
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.emitted().addToCart).toBeTruthy()
+    expect(wrapper.emitted().addToCart.length).toBe(1)
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
+  })
+})
