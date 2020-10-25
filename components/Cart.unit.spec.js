e84022e2f47e2726a6c3fb1fe31d6f2d97a3809d
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-10-25 01:23:03 -0300

    feature: implemented tests to cart component

--- components/Cart.unit.spec.js
+++ components/Cart.unit.spec.js
@@ -1,7 +1,18 @@
 import { mount } from '@vue/test-utils'
+import { makeServer } from '@/miragejs/server'
 import Cart from '@/components/Cart'
+import CartItem from '@/components/CartItem'
 
 describe('Cart', () => {
+  let server
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' })
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+
   it('should mount the component', () => {
     const wrapper = mount(Cart)
 
@@ -18,13 +29,13 @@ describe('Cart', () => {
     expect(wrapper.emitted().close).toHaveLength(1)
   })
 
-  it('should hide the cart when no prop isOpen is passed', async () => {
+  it('should hide the cart when no prop isOpen is passed', () => {
     const wrapper = mount(Cart)
 
     expect(wrapper.classes()).toContain('hidden')
   })
 
-  it('should hide the cart when prop isOpen is passed', async () => {
+  it('should hide the cart when prop isOpen is passed', () => {
     const wrapper = mount(Cart, {
       propsData: {
         isOpen: true,
@@ -32,5 +43,24 @@ describe('Cart', () => {
     })
 
     expect(wrapper.classes()).not.toContain('hidden')
+  })
+
+  it('should display "Cart is empty" when there ar no products', () => {
+    const wrapper = mount(Cart)
+
+    expect(wrapper.text()).toContain('Cart is empty')
+  })
+
+  it('should display 2 instances of CartItem when 2 products are provided', () => {
+    const products = server.createList('product', 2)
+
+    const wrapper = mount(Cart, {
+      propsData: {
+        products,
+      },
+    })
+
+    expect(wrapper.findAllComponents(CartItem)).toHaveLength(2)
+    expect(wrapper.text()).not.toContain('Cart is empty')
   })
 })
