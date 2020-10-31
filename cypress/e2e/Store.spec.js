02ee3f2ffe22b56d5bb57461b876b8737b9a098b
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-10-31 18:51:34 -0300

    feature: configured cypress and setup the first e2e test

---
+++ cypress/e2e/Store.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+context('Store', () => {
+  it('should display the store', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('body').contains('Brand')
+    cy.get('body').contains('Wrist Watch')
+  })
+})
