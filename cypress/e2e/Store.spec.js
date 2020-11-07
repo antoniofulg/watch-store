c56176e095513d7f6053b5b8a4265a5d55791a10
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-11-06 23:37:27 -0300

    fix: remove .only from tests

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -112,12 +112,12 @@ context('Store', () => {
       gid('cart-item').should('have.length', 0)
     })
 
-    it.only('should display quantity 1 when product is added to cart', () => {
+    it('should display quantity 1 when product is added to cart', () => {
       cy.addToCart({ index: 1 })
       gid('quantity').contains(1)
     })
 
-    it.only('should increase quantity when button + gets clicked', () => {
+    it('should increase quantity when button + gets clicked', () => {
       cy.addToCart({ index: 1 })
       gid('+').click()
       gid('quantity').contains(2)
@@ -125,7 +125,7 @@ context('Store', () => {
       gid('quantity').contains(3)
     })
 
-    it.only('should decrease quantity when button - gets clicked', () => {
+    it('should decrease quantity when button - gets clicked', () => {
       cy.addToCart({ index: 1 })
       gid('+').click()
       gid('+').click()
@@ -136,7 +136,7 @@ context('Store', () => {
       gid('quantity').contains(1)
     })
 
-    it.only('should not decrease below zero when button - gets clicked', () => {
+    it('should not decrease below zero when button - gets clicked', () => {
       cy.addToCart({ index: 1 })
       gid('-').click()
       gid('quantity').contains(0)
