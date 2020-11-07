f95cc8b71148f5f5f74af6c3196f7cac9b8de8bc
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-11-06 23:36:26 -0300

    feature: implemented e2e tests for item quantity on cart

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -111,6 +111,38 @@ context('Store', () => {
       gid('clear-cart-button').click()
       gid('cart-item').should('have.length', 0)
     })
+
+    it.only('should display quantity 1 when product is added to cart', () => {
+      cy.addToCart({ index: 1 })
+      gid('quantity').contains(1)
+    })
+
+    it.only('should increase quantity when button + gets clicked', () => {
+      cy.addToCart({ index: 1 })
+      gid('+').click()
+      gid('quantity').contains(2)
+      gid('+').click()
+      gid('quantity').contains(3)
+    })
+
+    it.only('should decrease quantity when button - gets clicked', () => {
+      cy.addToCart({ index: 1 })
+      gid('+').click()
+      gid('+').click()
+      gid('quantity').contains(3)
+      gid('-').click()
+      gid('quantity').contains(2)
+      gid('-').click()
+      gid('quantity').contains(1)
+    })
+
+    it.only('should not decrease below zero when button - gets clicked', () => {
+      cy.addToCart({ index: 1 })
+      gid('-').click()
+      gid('quantity').contains(0)
+      gid('-').click()
+      gid('quantity').contains(0)
+    })
   })
 
   context('Store > Product list', () => {
